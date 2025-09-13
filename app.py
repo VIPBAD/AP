4e47e40037df21b9f00f5a72768d13fc6bc4bbe4
sidhubad143 <lakhwinder4332@gmail.com>
2025-09-13 19:18:48 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 import os
-from flask import Flask, request, render_template, url_for
+from flask import Flask, request, render_template, jsonify, url_for
+import db
 
 app = Flask(__name__)
+db.init_db()
 
 @app.route("/")
 def home():
@@ -11,6 +13,30 @@ def home():
     thumb = request.args.get("thumb", url_for('static', filename='img/default_album.png'))
     artist = request.args.get("artist", "Unknown Artist")
     return render_template("player.html", audio_url=audio_url, title=title, thumb=thumb, artist=artist)
+
+@app.route("/me")
+def me():
+    uid = request.args.get("uid", "guest")
+    plays, favs = db.get_user_data(uid)
+    return render_template("profile.html", uid=uid, plays=plays, favs=favs)
+
+@app.route("/play", methods=["POST"])
+def mark_play():
+    data = request.get_json()
+    uid = data.get("uid", "guest")
+    song = data.get("song", "")
+    if song:
+        db.add_play(uid, song)
+    return jsonify({"status": "ok"})
+
+@app.route("/favorite", methods=["POST"])
+def mark_fav():
+    data = request.get_json()
+    uid = data.get("uid", "guest")
+    song = data.get("song", "")
+    if song:
+        db.add_favorite(uid, song)
+    return jsonify({"status": "ok"})
 
 if __name__ == "__main__":
     PORT = int(os.environ.get("PORT", 5050))
